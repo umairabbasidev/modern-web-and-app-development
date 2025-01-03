97a7b569eec1456faff8b8c6d4286257e7031bc2
muhammad umair <muhammadumairabbasi637@gmail.com>
2025-01-03 17:31:01 +0500

    javascript first assignment completed

---
+++ modern-web-and-app-development-assignments/javascriptAssignments/app.js
@@ -0,0 +1,106 @@
+// Chapter 1(ALERTS)
+
+// Q-1
+
+// alert("Hi, Welcome to my website.");
+
+// Q-2
+
+// alert("Error! Please enter a valid password.");
+
+// Q-3
+
+// alert("Welcome to JS Land...\nHappy Coding!");
+
+// Q-4
+
+// alert("Welcome to JS land...");
+// alert("Happy Coding!");
+
+// Q-5
+
+// console.log("Hello... I can run JS through my web browser's console.");
+
+// Q-6 continue at index page in script tag
+
+// chapter 1 exercises completed
+
+// Chapter 1(VARIABLES FOR STRINGS)
+
+// Q-1
+
+// let username;
+
+// Q-2
+
+// let myName = "Muhammad Umair Abbasi";
+
+// Q-3
+
+// let message;
+// message = "Hello World";
+// alert(message)
+
+// Q-4
+
+// let studentName = "Muhammad Umair Abbasi";
+// let studentAge = 22;
+// let studentEducation = "Certified Mobile Application Development";
+// alert("studentName: " + studentName)
+// alert("studentAge: " + studentAge + "years old")
+// alert("studentEducation: " + studentEducation)
+
+// Q-5
+
+// let str = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(str)
+
+// Q-6
+
+// let email = "muhammadumairabbasi637@gmail.com";
+// alert("My email address is " + email);
+
+// Q-7
+
+// let book = "Asmarter way to learn JavaScript.";
+// alert("I am trying to learn JS from the Book " + book);
+
+// Q-8
+
+// document.write("<h1>Yah! I can write HTML content through JavaScript</h1>");
+
+// Q-9
+
+// let str = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// console.log(str);
+// document.write(str)
+
+// chapter 2 exercises completed
+
+// Chapter 3(VARIABLES FOR NUMBERS)
+
+// Q-1
+
+// let age = 22;
+// alert("I am " + age + " years old");
+
+// Q-2
+
+// let visitorCount = 14;
+// alert("You have visited this site " + visitorCount + " times");
+
+// Q-3
+
+// let birthYear = 2002;
+// document.write(`<p>My birth year is ${birthYear}<br/>
+//     Data type of my declared variable is ${typeof birthYear}
+//     </p>`);
+
+// Q-4
+
+// let visitorName = "Muhammad Umair Abbasi";
+// let productTitle = "T-shirt";
+// let productQuantiy = 5;
+// document.write(`<p><strong>${visitorName}</strong> ordered <strong>${productQuantiy + ` ` + productTitle}</strong>(s) on XYZ Clothing store.</p>`);
+
+// ***************** Assignment 1 completed - (chapters 1 - 3) *****************
